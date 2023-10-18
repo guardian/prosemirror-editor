ae301b77eef762d0e7e2ad5c09ed6483fbf16550
Rhys Mills <rhys.mills@guardian.co.uk>
2023-10-18 16:28:46 +0100

    Include bundled styles

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,6 @@
 import { Editor } from "./Editor"; 
 import { RichTextEditor } from './RichTextEditor';
 import { fullFlatTextFieldConfig, fullMultiBlockTextFieldConfig, customMultiBlockTextConfig, customFlatTextConfig } from './richtext/config';
+import "./css/index.css";
 
 export { Editor, RichTextEditor, fullFlatTextFieldConfig, fullMultiBlockTextFieldConfig, customMultiBlockTextConfig, customFlatTextConfig };

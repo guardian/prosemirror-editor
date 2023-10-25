d2851e791879867812c0bc762125510f7317ed19
Rhys Mills <rhys.mills@guardian.co.uk>
2023-10-25 14:38:12 +0100

    Remove need for casting title as string

--- src/ts/richtext/menu.ts
+++ src/ts/richtext/menu.ts
@@ -5,9 +5,9 @@ import { Command, EditorState } from "prosemirror-state";
 import { isInNode, toggleBulletListCommand } from "./utils/listsHelpers";
 import { linkItemCommand, unlinkItemCommand } from "./utils/command-helpers";
 
-export const cmdItem = (cmd: Command, options: Partial<MenuItemSpec>) => {
+export const cmdItem = (cmd: Command, options: Partial<MenuItemSpec> & { title: string }) => {
   const passedOptions = {
-    label: options.title as string,
+    label: options.title,
     run: cmd,
     ...options
   };
@@ -22,8 +22,8 @@ export const markActive = (state: EditorState, type: MarkType) => {
   else return state.doc.rangeHasMark(from, to, type);
 };
 
-export const markItem = (markType: MarkType, options: Partial<MenuItemSpec>) => {
-  const passedOptions: Partial<MenuItemSpec> = {
+export const markItem = (markType: MarkType, options: Partial<MenuItemSpec> & { title: string } ) => {
+  const passedOptions: Partial<MenuItemSpec> & { title: string } = {
     active: (state: EditorState) => { return !!markActive(state, markType); },
     enable: () => true,
     ...options
@@ -31,30 +31,30 @@ export const markItem = (markType: MarkType, options: Partial<MenuItemSpec>) => 
   return cmdItem(toggleMark(markType), passedOptions);
 };
 
-const linkItem = (markType: MarkType, options: Partial<MenuItemSpec>) => {
+const linkItem = (markType: MarkType, options: Partial<MenuItemSpec> & { title: string }) => {
   const passedOptions: MenuItemSpec = {
     active: (state) => { return !!markActive(state, markType); },
     enable: (state) => { return !state.selection.empty; },
     run: linkItemCommand(markType),
-    label: options.title as string,
+    label: options.title,
     ...options
   };
   return new MenuItem(passedOptions);
 };
 
-const unlinkItem = (markType: MarkType, options: Partial<MenuItemSpec>) => {
+const unlinkItem = (markType: MarkType, options: Partial<MenuItemSpec> & { title: string }) => {
   const passedOptions: MenuItemSpec = {
     active: (state) => { return !!markActive(state, markType); },
     enable: (state) => { return !!markActive(state, markType); },
     run: unlinkItemCommand(markType),
-    label: options.title as string,
+    label: options.title,
     ...options
   };
   return new MenuItem(passedOptions);
 };
 
-const wrapListItem = (schema: Schema) => (nodeType: NodeType, options: Partial<MenuItemSpec>) => {
-  const passedOptions: Partial<MenuItemSpec> = {
+const wrapListItem = (schema: Schema) => (nodeType: NodeType, options: Partial<MenuItemSpec>  & { title: string }) => {
+  const passedOptions: Partial<MenuItemSpec>  & { title: string } = {
     active: (state) => { return isInNode(state, nodeType);},
     ...options
   };

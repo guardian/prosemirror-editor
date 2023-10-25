94d94929e1e0b8df49582753beceabbcdc2e285d
Samantha Gottlieb <samanthagottlieb31@gmail.com>
2023-10-25 12:52:48 +0100

    Remove vitejs react plugin to get storybook working
    Co-authored-by: Rhys Mills <34686302+rhystmills@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
   esbuild: {
     logOverride: { "this-is-undefined-in-esm": "silent" }
   },
